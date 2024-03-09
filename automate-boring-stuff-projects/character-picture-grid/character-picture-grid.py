7a7a4f2dcc68b044631cb62fb346314d758ae611
Sergey <50266067+torshin5ergey@users.noreply.github.com>
2024-03-09 17:32:46 +0300

    Add character-picture-grid.py

---
+++ automate-boring-stuff-projects/character-picture-grid/character-picture-grid.py
@@ -0,0 +1,19 @@
+# Character Picture Grid
+
+# Print the image using the symbols grid.
+def print_character(grid):
+	for i in grid:
+		print(j, end='') for j in i:
+		print()
+
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+print_character(grid)
