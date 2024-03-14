58e3e2c40699ed70fd82d872dc7b7d220a00a7d7
DuctopwH <webstor.38@gmail.com>
2024-03-15 00:22:24 +0300

    project finished

--- automate-boring-stuff-projects/tic-tac-toe/tic-tac-toe.py
+++ automate-boring-stuff-projects/tic-tac-toe/tic-tac-toe.py
@@ -1,5 +1,7 @@
 # Tic Tac Toe Game
 
+from random import choice
+
 # Game board
 board = {'topL': ' ', 'topM': ' ', 'topR': ' ', 'midL': ' ', 'midM':
 ' ', 'midR': ' ', 'botL': ' ', 'botM': ' ', 'botR': ' '}
@@ -11,62 +13,142 @@ def print_board(board: dict) -> None:
     print('-+-+-')
     print(board['botL'] + '|' + board['botM'] + '|' + board['botR'])
 
+def print_coordinates_format():
+    print(
+'''topL|topM|topR
+----+----+----
+midL|midM|midR
+----+----+----
+botL|botM|botR''')
+
 # Print "How to play" message
 def how_to_play() -> None:
     print('''\nHow to play\n'''
           '''1. When it's your turn, simply specify the coordinate where you want to place your mark. For example, "topL" corresponds to the top-left corner, "midM" - the middle in the middle, and so on.\n'''
           '''Coordinates are in accordance with the field below:\n''')
-    print('topL' + '|' + 'topM' + '|' + 'topR')
-    print('----+----+----')
-    print('midL' + '|' + 'midM' + '|' + 'midR')
-    print('----+----+----')
-    print('botL' + '|' + 'botM' + '|' + 'botR')
-    print('''\n2. The first player plays as "X", and the second player plays as "O"\n'''
+    print_coordinates_format()
+    print('''\n2. The first player plays as "O", and the second player plays as "X"\n'''
           '''3. The player who first fills a row, column, or diagonal with their marks wins.''')
 
 # Choose game mode: one or two players
-def choose_game_mode() -> int:
+def choose_from_menu() -> int:
+    print('Game menu:')
     while True:
         try:
-            mode = int(input('\nGame modes:\n1. One player\n2. Two players\nChoose mode (1/2): '))
-            if mode not in (1, 2):
+            mode = int(input('0. How To Play?\n1. One Player (PvE)\n2. Two Players (PvP)\nChoose the option (0/1/2): '))
+            if mode not in (0, 1, 2):
                 raise ValueError
             break
         except ValueError:
             print('\nError! Choose 1 or 2')
     return mode
 
-def check_turn():
-    pass
+def player_turn() -> str:
+    while True:
+        move = input().strip()
+        # Format user input according to coordinates values
+        formatted_move = move[:-1].lower() + move[-1].upper()
+        if (formatted_move) in board.keys():
+            if board[formatted_move] == ' ':
+                return formatted_move
+            else:
+                print("This space isn't empy.")
+        else:
+            print('Error! Coordinates must be entered in accordance with the field below:')
+            print_coordinates_format()
+        print(f'Try again. Move on which space?')
+
+def computer_random_move(moves) -> str:
+    random_move = choice(moves) # random.choice()
+    moves.remove(random_move)
+    return random_move
 
 def one_player():
-    pass
+    available_moves = ['topL', 'topM', 'topR', 'midL', 'midM', 'midR', 'botL', 'botM', 'botR']
+    print()
+    turn = 'O'
+    for _ in range(9): # Game loop
+        print_board(board)
+        print()
+        if turn == 'O':
+            move = computer_random_move(available_moves)
+            print(f"Computer's move is {move}")
+        else: 
+            print(f'Your turn. Move {turn} on which space?')
+            move = player_turn()
+            available_moves.remove(move)
+        board[move] = turn
+        
+        if is_win():
+            print_board(board)
+            if turn == 'O':
+                print(f'You beat the computer! Congratulation!')
+            else:
+                print(f'Computer beat you!')
+            return
+        
+        # Change player
+        if turn == 'O':
+            turn = 'X'
+        else:
+            turn = 'O'
+    print_board(board)
+    print('You tied.')
+    return
     
-def is_win():
-    pass
+def is_win() -> bool:
+    # Check win condition for rows
+    for row in ['top', 'mid', 'bot']:
+        if board[row+'L'] != ' ' and board[row+'L'] == board[row+'M'] == board[row+'R']:
+            return True
+    # Check win condition for cols
+    for col in ['L', 'M', 'R']:
+        if board['top'+col] != ' ' and board['top'+col] == board['mid'+col] == board['bot'+col]:
+            return True   
+    # Check win condition for diagonals
+    if board['topL'] != ' ' and board['topL'] == board['midM'] == board['botR']:
+        return True
+    if board['topR'] != ' ' and board['topR'] == board['midM'] == board['botL']:
+        return True
+    return False
 
 def two_players():
-    turn = 'X'
-    for _ in range(9):
+    print()
+    turn = 'O'
+    for _ in range(9): # Game loop
         print_board(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
-        check_turn()########################
+        print()
+        print(f'Turn for {turn}. Move on which space?')
+        move = player_turn()
         board[move] = turn
-        if turn == 'X':
-            turn = 'O'
-        else:
+
+        if is_win():
+            print_board(board)
+            print(f'Player "{turn}" win! Congratulation!')
+            return
+        
+        # Change player
+        if turn == 'O':
             turn = 'X'
+        else:
+            turn = 'O'
     print_board(board)
-    is_win()##########################
+    print('You tied.')
+    return
 
 def main():
-    print('\nWelcome to Tic-Tac-Toe Game!')
-    how_to_play()
-    if choose_game_mode() == 1:
-        one_player()
-    else:
-        two_players()
+    print('\nTic-Tac-Toe!\n')
+    while True: # Menu loop
+        user_input = choose_from_menu()
+        if user_input == 0:
+            how_to_play()
+            continue
+        elif user_input == 1:
+            one_player()
+        else:
+            two_players()
+        if input('Do you want to play again? (y/n): ').lower().strip() == 'y':
+            break
 
 
 if __name__ == '__main__':
