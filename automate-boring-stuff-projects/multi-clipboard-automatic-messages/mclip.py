a0c4d8fc5e1fca445411b40c7dfa1f5b72347679
DuctopwH <webstor.38@gmail.com>
2024-03-25 12:35:06 +0300

    Add -list -add -help

--- automate-boring-stuff-projects/multi-clipboard-automatic-messages/mclip.py
+++ automate-boring-stuff-projects/multi-clipboard-automatic-messages/mclip.py
@@ -19,14 +19,45 @@ def copy_top_clipboard(keyphrase: str) -> None:
         pyperclip.copy(TEXT[keyphrase])
         print(f'Text for {keyphrase} copied to clipborad.\n={TEXT[keyphrase]}=')
     except KeyError:
-        print(f'There is no text for {keyphrase}')
+        print(f'There is no text for {keyphrase}. Use -list to see available keyphrases')
+
+def print_commands_list():
+    print('Available keyphrases and messages:')
+    for k, v in TEXT.items():
+        print(f'  {k}: {v}')
+
+def add_keyphrase(new_key, new_value):
+    TEXT[new_key] = new_value
+    print(f'Keyphrase "{new_key}" with text "{new_value}" added successfully.')
+
+def print_help():
+    print('''Usage:
+    mclip [keyphrase] - copy phrase text
+    mclip -add [keyphrase] [text] - add a new keyphrase and its text
+    mclip -list - available keyphrases and messages
+    mclip -help - print this help message
+
+Examples:
+    mclip intro
+    mclip -list
+    mclip -add keyphrase TextInOneWord
+    mclip -add keyphrase "Text In Multiple Words"''')
 
 def main():
     if len(sys.argv) < 2:
-        print('Usage: python mclip.py [keyphrase] - copy phrase text')
+        print_help()
         sys.exit()
-
-    copy_top_clipboard(sys.argv[1])
+    if sys.argv[1] == '-help':
+        print_help()
+    elif sys.argv[1] == '-list':
+        print_commands_list()
+    elif sys.argv[1] == '-add':
+        if len(sys.argv) != 4:
+            print('Usage: mclip -add [keyphrase] [text]')
+            sys.exit()
+        add_keyphrase(sys.argv[2], sys.argv[3])
+    else:
+        copy_top_clipboard(sys.argv[1])
 
 
 if __name__ == "__main__":
