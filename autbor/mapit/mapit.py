ebef25769da192acb0c620ac8a3ec936bea74b95
DuctopwH <webstor.38@gmail.com>
2024-10-09 15:22:58 +0300

    Add Mapit.open_address()

--- autbor/mapit/mapit.py
+++ autbor/mapit/mapit.py
@@ -4,7 +4,7 @@ mapit.py - Launches a map in the browser using an address
 from the command line or clipboard.
 
 Usage:
-    mapit
+    mapit.py [ADDRESS]
 Example addresses:
 870 Valencia St, San Francisco, CA 94110
 
@@ -26,11 +26,17 @@ class Mapit:
     def __init__(self, address:str) -> None:
         self.address = address
         
+    def open_address(self):
+        map_url = "https://www.google.com/maps/place/"
+        webbrowser.open(map_url + self.address)
 
 if __name__ == "__main__":
+    # parse cli arguments
     if len(sys.argv) > 1:
-        address = " ".join(sys.argv[1:])
+        address = " ".join(sys.argv[1:])  # get args as a single string
     else:
-        address = pyperclip.paste()
+        address = pyperclip.paste()  # get address from clipboard
+    
     mapit = Mapit(address)
-    log.debug("%s", mapit.address)
+    log.debug("mapit address: %s", mapit.address)
+    mapit.open_address()
