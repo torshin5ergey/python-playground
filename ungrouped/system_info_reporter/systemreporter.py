9087301566bcbbf6ec28ea71a1cfe1c06aba84aa
torshin5ergey <ttorshin5ergey@gmail.com>
2025-02-26 23:34:57 +0300

    feat(system_info_reporter): add report method

--- ungrouped/system_info_reporter/systemreporter.py
+++ ungrouped/system_info_reporter/systemreporter.py
@@ -25,26 +25,40 @@ class SystemReporter:
         self.cpu_info = {}
         self.disk_info = {}
 
+
+    def report(self, param):
+        if param == "cpu":
+            log.info(json.dumps(self.cpu_info))
+            return json.dumps(self.cpu_info, indent=4)
+        pass
+
+
     def print_cpu_info(self):
         if not self.cpu_info:
-            log.error("No CPU information available.")
+            log.error("No CPU information available")
             return
         print("CPU information:")
         for key, value in self.cpu_info.items():
             print(f"{key}\t: {value}")
 
+
     def get_cpu_model_name(self):
+        # TODO
         pass
+
 
     def get_cpu_cores(self):
         with open("/proc/cpuinfo", "r") as f:
             cpuinfo = f.read()
+
         cpu_cores = len(re.findall(r"processor\s+: \d+", cpuinfo))
         if not cpu_cores:
-            log.error()
-            sys.exit(1)
+            log.error("Could not determine the number of CPU cores")
+            raise ValueError("CPU cores information not found.")
+
         self.cpu_info["cores"] = cpu_cores
         log.info(f"CPU cores: {self.cpu_info['cores']}")
+
 
     def get_cpu_threads(self):
         with open("/proc/cpuinfo", "r") as f:
@@ -53,14 +67,14 @@ class SystemReporter:
         if not cpu_threads:
             log.error()
             sys.exit(1)
-
-    # print(json.dumps(system, indent=4))
+        # TODO add to cpu_info
 
 
 def main():
     reporter = SystemReporter()
     reporter.get_cpu_cores()
     reporter.print_cpu_info()
+    reporter.report("cpu")
 
 if __name__ == "__main__":
     main()
