6ae4000d47502782db50ae5f88da015016ee5487
torshin5ergey <ttorshin5ergey@gmail.com>
2025-03-04 13:39:15 +0300

    feat(opening_all_search_results): add searchpypi.py

---
+++ autbor/opening_all_search_results/README.md
@@ -0,0 +1,16 @@
+# Opening All Search Results
+
+## Description
+
+This program does:
+1. Gets search keywords from the command line arguments
+2. Retrieves the search results page
+3. Opens a browser tab for each results
+
+## Usage
+
+
+
+## Author
+
+Sergey Torshin [@torshin5ergey](https://github.com/torshin5ergey)
---
+++ autbor/opening_all_search_results/searchpypi.py
@@ -0,0 +1,15 @@
+#! python3
+# searchpypi.py - Opens several search results
+
+import requests
+import sys
+import bs4
+import webbrowser
+
+print("Searching...")
+res = requests.get("https://google.com/search?q=" "https://pypi.org/search/?q=" + " ".join(sys.argv[1:]))
+res.raise_for_status()
+
+# TODO: Retrieve top search result links
+
+# TODO: Open a browser tab for each result
