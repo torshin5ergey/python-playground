88d9af09bad906ebfa4fac9504003e829c2bf96b
torshin5ergey <ttorshin5ergey@gmail.com>
2025-03-04 13:47:33 +0300

    docs(opening_all_search_results): add step description

--- autbor/opening_all_search_results/searchpypi.py
+++ autbor/opening_all_search_results/searchpypi.py
@@ -6,6 +6,7 @@ import sys
 import bs4
 import webbrowser
 
+# Get the command line arguments and request the search page
 print("Searching...")
 res = requests.get("https://google.com/search?q=" "https://pypi.org/search/?q=" + " ".join(sys.argv[1:]))
 res.raise_for_status()
