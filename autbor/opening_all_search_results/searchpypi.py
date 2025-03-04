44844fefb95e3753b03e866870342c724f75f6f8
torshin5ergey <ttorshin5ergey@gmail.com>
2025-03-04 13:51:54 +0300

    feat(opening_all_search_results): add search result parsing

--- autbor/opening_all_search_results/searchpypi.py
+++ autbor/opening_all_search_results/searchpypi.py
@@ -11,6 +11,8 @@ print("Searching...")
 res = requests.get("https://google.com/search?q=" "https://pypi.org/search/?q=" + " ".join(sys.argv[1:]))
 res.raise_for_status()
 
-# TODO: Retrieve top search result links
+# Retrieve top search result links
+soup = bs4.BeautifulSoup(res.text, "html.parser")
 
 # TODO: Open a browser tab for each result
+linkElems = soup.select(".package-snippet")
